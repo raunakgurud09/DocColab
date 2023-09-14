0575d71624c2ffd248c61bde7e73815967dc3191
Raunak Gurud <raunakgurud2121@gmail.com>
2023-09-14 15:54:01 +0530

    fix: settings page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import { buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>home</div>
+    <div className='p-40'>
+      <p>home</p>
+      <Link href={'/dashboard'} className={buttonVariants()}>Dashboard</Link>
+    </div>
   )
 }
