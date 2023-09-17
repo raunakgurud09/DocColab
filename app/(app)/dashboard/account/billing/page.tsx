b5f936db48f4fab3729daf529e33d99240a4ed46
Raunak Gurud <raunakgurud2121@gmail.com>
2023-09-17 16:50:22 +0530

    style: alert added

--- app/(app)/dashboard/account/billing/page.tsx
+++ app/(app)/dashboard/account/billing/page.tsx
@@ -1,4 +1,6 @@
 import AccountHeader from "@/components/Common/AccountHeader";
+import { Icons } from "@/components/icons";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function page() {
   return (
@@ -6,7 +8,15 @@ export default function page() {
       heading="Billing"
       text="Manage billing and your subscription plan."
     >
-      billing
+      <div className="flex flex-col">
+        <Alert className="bg-orange-800/60 border-orange-700">
+          <Icons.warning className="h-4 w-4" />
+          <AlertTitle>This is a demo app.</AlertTitle>
+          <AlertDescription>
+          Docolab app is a demo app using a Stripe test environment. You can find a list of test card numbers on the Stripe docs.
+          </AlertDescription>
+        </Alert>
+      </div>
     </AccountHeader>
   )
 }
--- components/Common/AccountHeader.tsx
+++ components/Common/AccountHeader.tsx
@@ -8,8 +8,8 @@ export default function AccountHeader({ heading, text, children }: {
   return (
     <div className='flex flex-col space-y-8 px-0 md:px-4 lg:px-8'>
       <div className='flex flex-col '>
-        <h1 className='text-lg font-medium'>{heading}</h1>
-        <p className='text-xs opacity-80'>{text}</p>
+        <h1 className='text-xl font-medium'>{heading}</h1>
+        <p className='text-sm opacity-80'>{text}</p>
       </div>
       <div className=''>{children}</div>
     </div>
