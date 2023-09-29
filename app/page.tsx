7fdfcffe3f6a7041d00c39628e1940e4781c04ad
Raunak Gurud <raunakgurud2121@gmail.com>
2023-09-29 21:17:24 +0530

    style: creating all pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import Cta from '@/components/Home/Cta'
+import Features from '@/components/Home/Features'
+import Hero from '@/components/Home/Hero'
 import HomeNavbar from '@/components/Home/Navbar'
+import OSS from '@/components/Home/Oss'
+import Testimonials from '@/components/Home/Testimonials'
+import WorkingVideo from '@/components/Home/WorkingVideo'
 import { buttonVariants } from '@/components/ui/button'
 import Link from 'next/link'
 
@@ -6,8 +12,14 @@ export default function Home() {
   return (
     <div className=''>
       <HomeNavbar />
-      <p>home</p>
-      <Link href={'/dashboard'} className={buttonVariants()}>Dashboard</Link>
+      <div className='max-w-7xl mx-auto px-4 flex  flex-col justify-center items-center h-full'>
+        <Hero />
+        <Cta />
+        <Features />
+        <Testimonials />
+        <WorkingVideo />
+        <OSS />
+      </div>
     </div>
   )
 }
--- components/Home/Navbar.tsx
+++ components/Home/Navbar.tsx
@@ -24,16 +24,16 @@ const navList = [
 
 export default function HomeNavbar() {
   return (
-    <header className="select-none backdrop-blur sticky z-50 border-b border-white/[0.1] h-12 top-0 ">
+    <header className="select-none backdrop-blur sticky z-50 border-b border-foreground/[0.1] h-12 top-0 ">
       <div className="max-w-7xl mx-auto px-4 flex justify-between items-center h-full">
         <div className="flex space-x-4">
           <Logo />
           <nav className="h-full">
-            <ul className="flex space-x-4 text-white/60 hover:text-white/80">
+            <ul className="flex space-x-6 text-white/60 hover:text-white/80">
               {
                 navList.map((nav, i) => (
                   <li key={i}>
-                    <Link className="text-white/60 hover:text-white/80 text-sm font-normal" href={nav.href}>
+                    <Link className="text-foreground hover:text-foreground/80  text-sm" href={nav.href}>
                       {nav.name}
                     </Link>
                   </li>
@@ -43,7 +43,7 @@ export default function HomeNavbar() {
           </nav>
         </div>
         <div>
-          <a href="http://localhost:3000/dashboard" className="px-2 py-[2px] rounded-md text-sm text-white/60 hover:text-white/80">Login</a>
+          <a href="http://localhost:3000/dashboard" className="px-2 py-[2px] rounded-md text-sm text-foreground/90 hover:text-foreground">Login</a>
         </div>
       </div>
     </header>
